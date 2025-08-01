ffda7faa74021ea3c1d07edabf185a5284bea656
Donn4try <kanggayang01@gmail.com>
2025-08-01 16:10:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function bloomFlower() {
   if (document.body.classList.contains('not-loaded')) {
     document.body.classList.remove("not-loaded");
     document.querySelector('.reloadBtn').textContent = 'Kuncup Bunga';
+    
+    const music = document.getElementById("bg-music");
+    music.play().catch(err => console.log("Autoplay diblokir: ", err));
   } else {
     location.reload();
   }
