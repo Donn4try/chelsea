e0d51df73fcb7134863c683d1989a489c7a3d8b7
Donn4try <kanggayang01@gmail.com>
2025-08-01 15:32:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function bloomFlower() {
   if (document.body.classList.contains('not-loaded')) {
     document.body.classList.remove("not-loaded");
-    document.querySelector('.reloadBtn').textContent = 'Reload Page';
+    document.querySelector('.reloadBtn').textContent = 'Kuncup Bunga';
   } else {
     location.reload();
   }
